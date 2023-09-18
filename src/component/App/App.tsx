dbc80a1c249c0447f2aac11c98b2ec7c5b72e9c9
Gabriel Filipy da Silva Correa <gabriel.correa@senacam.com.br>
2023-09-18 19:29:02 -0400

    Create component button

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from '../Button/Button';
 
 function App() {
   return (
     <>
-      
+      <Button value={'Cadastrar'} />
+      <Button value={'Editar'} />  
+     
     </>
   );
 }
