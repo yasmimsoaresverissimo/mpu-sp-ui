dda6ef7e964c5035568051c24d237bc5be36c7de
devEdesonmota <edesonmota0@gmail.com>
2023-09-25 18:09:33 -0400

    alter

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -5,7 +5,6 @@ import { Route,BrowserRouter,Routes } from 'react-router-dom';
 import Documento from '../Documento/Documento';
 import Login from '../Login/Login';
 
-
 function App() {
   return (
     <div className="App">
