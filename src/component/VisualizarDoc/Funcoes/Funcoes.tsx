660dc8cac202ef25b0a9e384eb70ef9b4ce6f37a
arrecadaçãoFreitas <freitassilva82@gmail.com>
2023-10-03 18:42:50 -0300

    Terminando Finalizar Documento

--- src/component/VisualizarDoc/Funcoes/Funcoes.tsx
+++ src/component/VisualizarDoc/Funcoes/Funcoes.tsx
@@ -3,8 +3,29 @@ import '../VisualizarDoc.css'
 import Button from "../../../compenentes-compartilhados/Button/Button"; 
 import { Grid } from "@mui/material";
 import { deflate } from "zlib";
-
+import Swal from 'sweetalert2'
 function Funcoes() {
+    const Excluir=()=>{
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes,  delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+            }
+          })
+    }
+
     return <div className="AppFucoes"> 
         <Grid container spacing={0.5}>
             <Grid item xs={1.4}>
@@ -17,7 +38,7 @@ function Funcoes() {
                 <Button value="Editar"/>
                 </Grid>
                 <Grid item xs={1.4}> 
-                <Button value="Excluir"/>
+                <Button value="Excluir"onClick={Excluir}/>
                 </Grid>
                 <Grid item xs={2}> 
                 <Button value="Incluir Cossignatário"/>
