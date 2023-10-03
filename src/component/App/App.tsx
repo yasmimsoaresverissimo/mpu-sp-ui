336db151541a3a37d08309eb1225981f21f06913
mardemfleury <mardemprojetoedesign@hotmail.com>
2023-10-03 19:21:26 -0300

    Redirecionamento mesa virtual

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
         <Routes>
+          <Route path="/" element={<Mesa />} ></Route>
           <Route path="/mesa-virtual" element={<Mesa />} ></Route>
           <Route path="/documento" element={<Documento />} ></Route>
           <Route path="/usuario" element={<Formulario />} ></Route>
