038d1e904330325360e0d0ce48a79086e0ce3b35
yasmimsoaresverissimo <yasmim.soares.verissimo@gmail.com>
2024-04-29 18:09:54 -0400

    Tornando dinâmico

--- src/scriptCadastroDocumento.js
+++ src/scriptCadastroDocumento.js
@@ -1,20 +1,31 @@
-
 const documentoForm = document.getElementById('documentoForm');
 const responseDiv = document.getElementById('response');
 
-const data = {
-  Assunto: ''
-};
-
 documentoForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  data.Assunto = event.target.Assunto.value;
+
+  let descricao = ''; 
+
+  // Itera sobre todos os elementos do formulário
+  for (const input of documentoForm.elements) {
+  // Verifica se o elemento é um input ou textarea e tem um ID e um valor
+  if ((input.tagName === 'INPUT' || input.tagName === 'TEXTAREA') && input.id && input.value) {
+  // Adiciona o ID e o valor do input à descrição
+  descricao += `${input.id}: ${input.value};`;
+  }
+}
+    const data = {
+      descricao: descricao
+    };
+
   const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://localhost:8082/v1/auth/login');
+  xhr.open('POST', 'http://localhost:8085/v1/document/cadastrar');
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onload = () => {
     if (xhr.status === 201) {
       responseDiv.textContent = xhr.responseText;
+      testForm.reset();
+      alert("Formulário enviado com sucesso!");
     } else {
   
     }
